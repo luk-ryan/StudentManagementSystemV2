c830dded94d97793895dd53ba2013f3814092971
luk-ryan <ryankluk@my.yorku.ca>
2022-11-30 17:06:33 -0500

    added register page

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ def hello_world():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")

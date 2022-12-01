95fa81264c564114662efa745e225d4ec71e7384
luk-ryan <ryankluk@my.yorku.ca>
2022-12-01 14:35:29 -0500

    Cleaned up html code and added base student html page

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 # Automatically reload server when template files are changed
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+# Directories for the home page
 @app.route("/")
-def hello_world():
+@app.route("/home")
+def home():
     return render_template("home.html")
 
-@app.route("/login")
+# Directory for login page
+@app.route("/login", methods = ["POST","GET"])
 def login():
     return render_template("login.html")
 
+# Directory for register page
 @app.route("/register")
 def register():
     return render_template("register.html")
 
-@app.route("/home")
-def home():
-    return render_template("home.html")
+# Directory for the Student page
+@app.route("/student")
+def student():
+    return render_template("student.html")

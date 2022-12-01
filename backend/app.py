82670625115ada2addccc58aa2f52a7d16989e20
luk-ryan <ryankluk@my.yorku.ca>
2022-12-01 16:23:01 -0500

    finished link to student home page and added messaging when logging in/out

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 
 app = Flask(__name__)
 app.secret_key = "Hello World!"
@@ -25,6 +25,7 @@ def register():
     if request.method == "POST":
         first_name = request.form["fName"] # temporary stores first name field
         session["student_session"] = first_name # stores the above field in session
+        flash(f" Logged in Successfully!") # messaging tells user they have been logged in
         return redirect(url_for("student")) # redirects to student page
 
     # Default register page 
@@ -38,14 +39,21 @@ def student():
     # this is the default Student home page once they log in
     if "student_session" in session:
         s = session["student_session"]
-        return f"<h1>{s}</h1>" # temporary to pass input through and check if it works
+        return render_template("student.html", student = s)
     
     # redirects back to login if they are not logged in the session
     else:
+        flash(f"You are not logged in")
         return redirect(url_for("login"))
 
 # Log out of a session
 @app.route("/logout")
 def logout():
-    session.pop("student_session", None)
+
+    # messaging to let user know they have been logged out
+    if "student_session" in session:
+        s = session["student_session"]
+        flash(f" {s} has been logged out Successfully", "info")
+
+    session.pop("student_session", None)    
     return redirect(url_for("login"))
